95fbdfe8ee0df25846861e3a3deafe4f6a45e180
noguzdogan <72796743+noguzdogan@users.noreply.github.com>
2020-10-15 23:39:01 +0300

    Update Satir_Kaydirma.py

--- Satir_Kaydirma.py
+++ Satir_Kaydirma.py
@@ -4,10 +4,10 @@
 # 1: \n ifadesini direkt kullanarak
 print("First Line\nSecond Line")
 
-# 2: Son satırı 'end =' ile belirleyerek ('end = ()'in hemen sağına virgül atıp yeni satır ekleyemiyorum)
+# 2: Son satırı 'end =' ile belirleyerek ('end= "..." 'in hemen sağına virgül atıp yeni satır ekleyemiyorum)
 print("\n\n") # sadece satırlar arasına mesafe koymak için ekledim
 print("1st Line", end = "\n2nd Line")
 
-# 3: Satırları 'sep =' ifadesiyle ayırıp ona '\n' değerini atayarak (öncekine göre daha çok satır ayırabiliyorum)
+# 3: Satırları 'sep= "..." ' ifadesiyle ayırıp ona '\n' değerini atayarak (öncekine göre daha çok satır ayırabiliyorum)
 print("\n\n")
 print("İlk Satır","İkinci Satır","Üçüncü Satır", sep="\n")

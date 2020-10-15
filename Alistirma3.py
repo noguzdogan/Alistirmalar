81282e70aa32349cd6c17184d04e986e17bc2261
noguzdogan <72796743+noguzdogan@users.noreply.github.com>
2020-10-15 17:49:05 +0300

    Update Alistirma3.py

--- Alistirma3.py
+++ Alistirma3.py
@@ -1,12 +1,10 @@
 # range 18'den sonrası için hep aynı sonucu verdiği için kısa sürede sonuç
 # elde etmek adına range'in durduğu noktayı 18 olarak belirledim
-toplam=0
 def faktoriyel(i):
     if i==0:
         return 1
     else:
         return i * faktoriyel(i-1)
 for i in range(0,18):
-    toplam += 1/faktoriyel(i)
-value_exp = toplam
+    value_exp += 1/faktoriyel(i)
 print("e sayısının yaklaşık değeri: ",value_exp)

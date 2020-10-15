ed6cc308f55130898efa83143c45f305ae109149
noguzdogan <72796743+noguzdogan@users.noreply.github.com>
2020-10-15 17:50:36 +0300

    Update Alistirma3.py

--- Alistirma3.py
+++ Alistirma3.py
@@ -1,5 +1,6 @@
 # range 18'den sonrası için hep aynı sonucu verdiği için kısa sürede sonuç
 # elde etmek adına range'in durduğu noktayı 18 olarak belirledim
+value_exp = 0
 def faktoriyel(i):
     if i==0:
         return 1

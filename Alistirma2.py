6541b2d4e9c34e992a3a8d4817cae00934a2d145
Naip Oğuz Doğan <72796743+noguzdogan@users.noreply.github.com>
2020-11-06 02:25:56 +0300

    Update Alistirma2.py

--- Alistirma2.py
+++ Alistirma2.py
@@ -1,9 +1,6 @@
 # inf değerini standart modülleri kullanarak 'int' tipine dönüştüremediğim gibi
-# 'float' tipindeki verileri for döngüsü içine alamadım. Bu yüzden int32'nin max
-# değerini baz alarak for döngüsü oluşturdum. Ancak programın daha hızlı son-
-# lanabilmesi için bu değeri biraz küçülttüm.
+# 'float' tipindeki verileri for döngüsü içine alamadım.
 
-# Biraz uzun sürüyor ama yakın bir sonuç veriyor :)
 import math
 sol_taraf = 0
 for x in range(1,10000):
